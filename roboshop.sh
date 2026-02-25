be897dafabe1021abb955cf0d794032e9758b672
kiran <bkiran1407@gmail.com>
2026-02-25 12:44:49 +0530

    Create roboshop.sh

---
+++ roboshop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+SG_ID="sg-05e225318bf0878b4"
+AMI_ID="ami-0220d79f3f480ecf5"
+
+for instance in $@
+do
+    instance_id = $(aws ec2 run-instances \
+    --image-id $AMI_ID \
+    --instance-type t3.micro \
+    --security-group-ids $SG_ID \
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+    --query 'Reservations[0].Instances[0].privateIpAddress' \
+    --output text)
+
+    if [ $instance == "frontend" ]; then
+        IP=$(
+            aws ec2 describe-instance \
+            --instance-ids $instance_id \
+            --query 'Reservations[0].Instances[0].publicIpAddress' \
+            --output text
+        )
+    else
+        IP=$(
+            aws ec2 describe-instance \
+            --instance-ids $instance_id \
+            --query 'Reservations[0].Instances[0].privateIpAddress' \
+            --output text
+        )   
+    fi     
+done
