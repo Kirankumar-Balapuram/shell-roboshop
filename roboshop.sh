0dd46c71ac0f30c310bfd2031bda55e4b322f1a7
kiran <bkiran1407@gmail.com>
2026-02-25 14:42:40 +0530

    Update roboshop.sh

--- roboshop.sh
+++ roboshop.sh
@@ -1,33 +1,53 @@
 #!/bin/bash
-
+set -e
 SG_ID="sg-05e225318bf0878b4"
 AMI_ID="ami-0220d79f3f480ecf5"
-
-for instance in $@
-do
-    INSTANCE_ID=$( aws ec2 run-instances \
-    --image-id $AMI_ID \
-    --instance-type t3.micro \
-    --security-group-ids $SG_ID \
-    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
-    --query 'Instances[0].InstanceId' \
-    --output text )
-
-    if [ $instance == "frontend" ]; then
+ 
+for instance in "$@"; do
+    echo "Processing instance: $instance"
+ 
+    EXISTING_INSTANCE_ID=$(
+        aws ec2 describe-instances \
+        --filters "Name=tag:Name,Values=$instance" \
+                  "Name=instance-state-name,Values=pending,running,stopped,stopping" \
+        --query 'Reservations[].Instances[].InstanceId' \
+        --output text
+    )
+ 
+    if [ -n "$EXISTING_INSTANCE_ID" ]; then
+        echo "Instance '$instance' already exists ($EXISTING_INSTANCE_ID). Skipping creation."
+        INSTANCE_ID="$EXISTING_INSTANCE_ID"
+    else
+        echo "Creating instance '$instance'..."
+ 
+        INSTANCE_ID=$(
+            aws ec2 run-instances \
+            --image-id "$AMI_ID" \
+            --instance-type "t3.micro" \
+            --security-group-ids "$SG_ID" \
+            --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
+            --query 'Instances[0].InstanceId' \
+            --output text
+        )
+    fi
+ 
+    aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+ 
+    if [ "$instance" = "frontend" ]; then
         IP=$(
-            aws ec2 wait instance-running \
-            --instance-ids $INSTANCE_ID  \
-            --query 'Reservations[].Instances[].publicIpAddress' \
+            aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[0].Instances[0].PublicIpAddress' \
             --output text
         )
     else
         IP=$(
-            aws ec2 wait instance-running \
-            --instance-ids $INSTANCE_ID \
-            --query 'Reservations[].Instances[].privateIpAddress' \
+            aws ec2 describe-instances \
+            --instance-ids "$INSTANCE_ID" \
+            --query 'Reservations[0].Instances[0].PrivateIpAddress' \
             --output text
-        )   
+        )
     fi
-    echo "IP Address: $IP"
-    
-done   
+ 
+    echo "IP Address $instance: $IP"
+ done
