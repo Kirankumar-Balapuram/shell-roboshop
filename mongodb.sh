94376f0837e9fd15413bcc36cfa261f9377614b4
kiran <bkiran1407@gmail.com>
2026-02-26 10:22:28 +0530

    Update mongodb.sh

--- mongodb.sh
+++ mongodb.sh
@@ -27,17 +27,17 @@ fi
 cp mongo.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying Mongo Repo"
 
-dnf install mongodb-org -y
+dnf install mongodb-org -y &>>$LOGS_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl enable mongod
+systemctl enable mongod &>>$LOGS_FILE
 VALIDATE $? "Enable MongoDB"
 
-systemctl start mongod
+systemctl start mongod &>>$LOGS_FILE
 VALIDATE $? "Start MongoDB"
 
 sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
 VALIDATE $? "Allowing remote connections"
 
-systemctl restart mongod
+systemctl restart mongod &>>$LOGS_FILE
 VALIDATE $? "Restart MongoDB"
