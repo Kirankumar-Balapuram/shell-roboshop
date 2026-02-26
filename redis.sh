3ddf1b3b22d28d6edf96a21c9b0bbd6e2d947a07
kiran <bkiran1407@gmail.com>
2026-02-26 12:34:29 +0530

    Create redis.sh

---
+++ redis.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.balapuram.online
+
+if [ $USERID -ne 0 ]; then
+    echo -e "$R please run this script with root user access"
+    exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 ... $R Failure" | tee -a $LOGS_FILE
+    exit 1
+else
+    echo -e "$2 ... $G Success" | tee -a $LOGS_FILE
+fi   
+}
+
+dnf module disable redis -y &>>$LOGS_FILE
+dnf module enable redis:7 -y &>>$LOGS_FILE
+VALIDATE $? "Enable Redis:7"
+
+dnf install redis -y &>>$LOGS_FILE
+VALIDATE $? "installed Redis"
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode on' /etc/redis/redis.conf
+VALIDATE $? "Allowing remote connections"
+
+systemctl enable redis &>>$LOGS_FILE
+systemctl start redis
+VALIDATE $? "Enabled and started Redis"
